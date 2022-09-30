9bba316cdfa59d5f0788e662e580eff0686e0a29
Jinrui Chen <jinrui@kooboo.cn>
2022-09-30 18:09:19 +0800

    通过 plugin-vue 支持 SPC 语法

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 
-import SButton from "./button";
+// import SButton from "./button";
+import SFCButton from "./SFCButton.vue";
 
-createApp(SButton).mount("#app");
+createApp(SFCButton).mount("#app");
