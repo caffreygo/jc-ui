46be008497db639ad35aaac7a00cc33bfee5fb66
Jinrui Chen <jinrui@kooboo.cn>
2022-10-08 09:57:20 +0800

    通过 plugin-vue-jsx 支持 JSX

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 
 // import SButton from "./button";
-import SFCButton from "./SFCButton.vue";
+import JSXButton from "./JSXButton";
 
-createApp(SFCButton).mount("#app");
+createApp(JSXButton).mount("#app");
