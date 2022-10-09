cfe7bee6c9d4d438eb205ef8dc528a9ee465319b
Jinrui Chen <jinrui@kooboo.cn>
2022-10-09 12:02:46 +0800

    temp

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import DefaultTheme from "vitepress/theme";
-import SmartyUI from "../../../src/entry";
+import JcUI from "../../../src/entry";
 import 'vitepress-theme-demoblock/theme/styles/index.css'
 import Demo from "vitepress-theme-demoblock/components/Demo.vue";
 import DemoBlock from "vitepress-theme-demoblock/components/DemoBlock.vue";
@@ -7,7 +7,7 @@ import DemoBlock from "vitepress-theme-demoblock/components/DemoBlock.vue";
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    app.use(SmartyUI);
+    app.use(JcUI);
     app.component("Demo", Demo);
     app.component("DemoBlock", DemoBlock);
   },
