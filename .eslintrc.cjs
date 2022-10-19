de9471cf589496b12c1d710278ed94e9faf76ab6
Jerry Chen <caffreygo@163.com>
2022-10-19 23:28:38 +0800

    husky pre-commit for lint checking

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,20 +12,15 @@ module.exports = {
     __DEV__: true,
   },
   // 解析 .vue 文件
-  parser: "vue-eslint-parser",
-  extends: [
-    "plugin:json/recommended",
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/prettier",
-  ],
-  plugins: ["@typescript-eslint"],
+  parser: 'vue-eslint-parser',
+  extends: ['plugin:json/recommended', 'plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    parser: "@typescript-eslint/parser", // 解析 .ts 文件
+    parser: '@typescript-eslint/parser', // 解析 .ts 文件
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": "error",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'error',
   },
-};
+}
--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, h } from 'vue'
 import 'uno.css'
 
 export type ISize = 'small' | 'medium' | 'large'
