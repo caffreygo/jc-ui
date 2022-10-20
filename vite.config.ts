78121bd1d71915601907478e8db744f6161b733f
Jerry Chen <caffreygo@163.com>
2022-10-20 22:33:52 +0800

    build: 打包配置 & sourcemap & iife demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import Unocss from "./config/unocss";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import Unocss from './config/unocss'
 
 // https://vitejs.dev/config/
 
 const rollupOptions = {
-  external: ["vue", "vue-router"],
+  external: ['vue'],
   output: {
+    // for umd/iffe
     globals: {
-      vue: "Vue",
+      vue: 'Vue',
     },
   },
-};
+}
 
 export default defineConfig({
   plugins: [vue(), vueJsx(), Unocss()],
@@ -22,7 +23,7 @@ export default defineConfig({
     globals: true,
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
-    environment: "happy-dom",
+    environment: 'happy-dom',
     // 支持tsx组件，很关键
     transformMode: {
       web: [/.[tj]sx$/],
@@ -30,13 +31,15 @@ export default defineConfig({
   },
   build: {
     rollupOptions,
-    minify: false,
+    minify: 'terser', // boolean | 'terser' | 'esbuild'
+    sourcemap: true, // 输出单独 source文件
     cssCodeSplit: true,
+    reportCompressedSize: true,
     lib: {
-      entry: "./src/entry.ts",
-      name: "JcUI",
-      fileName: "jc-ui",
-      formats: ["es", "umd", "iife"],
+      entry: './src/entry.ts',
+      name: 'JcUI',
+      fileName: 'jc-ui',
+      formats: ['es', 'umd', 'iife'], // 导出模块类型
     },
   },
-});
+})
